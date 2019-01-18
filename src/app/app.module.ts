cb9441af778acccc26381898bb538a87e4e3338b
Neeraj Negi <neeraj.negi174@gmail.com>
2019-01-18 21:44:29 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { LoginModule } from './modules/login/login.module';
 
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    DashboardModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
