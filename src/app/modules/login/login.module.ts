b3e34879b8b4ab0ab270db4ada05f49522f8d011
Neeraj Negi <neeraj.negi174@gmail.com>
2019-01-20 14:24:59 +0530

    Routing, Fixed CSS, Login interaction

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
 import { LoginRoutingModule } from './login-routing.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    FormsModule
   ],
   declarations: [LoginComponent],
   exports: [LoginComponent]
--- src/app/modules/login/pages/login/login.component.ts
+++ src/app/modules/login/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  login(event: Event): void {
+    event.preventDefault();
+    this.email = this.email.trim();
+    this.password = this.password.trim();
+    if(this.email === "everyulb" && this.password === "everyulb"){
+      this.router.navigate(['dashboard']);
+    } else {
+      alert('Wrong Credentials');
+    }
   }
 
 }
