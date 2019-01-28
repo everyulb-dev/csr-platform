9575d9f72b3fb744e22e2fa38655f65ab88e3728
Neeraj Negi <neeraj.negi174@gmail.com>
2019-01-28 10:44:07 +0530

    Project Progress accordian added

--- src/app/modules/project/components/project-progress/project-progress.component.ts
+++ src/app/modules/project/components/project-progress/project-progress.component.ts
@@ -10,6 +10,64 @@ export class ProjectProgressComponent implements OnInit {
   isLinear = false;
   panelOpenState = false;
   
+  steps = [
+    {
+      stepName: 'Orientation Discussion',
+      tasks: [
+        'Open meeting at village level',
+        'Village representative and government officials meeting'
+      ]
+    },
+    {
+      stepName: 'Transect Walk',
+      tasks: [
+        'Baseline survey',
+        'Primary data collection-QUestion asked to villagers',
+        'Identifying key issue areas(Litering places, dumpings site, pics and geotag)',
+      ]
+    },
+    {
+      stepName: 'Current system & Need analysis',
+      tasks: [
+        'Current / Existing SWM system',
+        'Waste generation',
+        'Waste Characteristics',
+        'Need analysis - Required facility / Machinery etc.'
+      ]
+    },
+    {
+      stepName: 'Proposed SWM system',
+      tasks: [
+        'Preparation of DPR for current system',
+      ]
+    },
+    {
+      stepName: 'Approval of DPR in open meeting',
+      tasks: [
+        'DPR get approved in open meeting with villagers',
+      ]
+    },
+    {
+      stepName: 'Technical Appraisal',
+      tasks: [
+        'Technical Appraisal of DPR',
+      ]
+    },
+    {
+      stepName: 'Financial Appraisal',
+      tasks: [
+        'Financial Appraisal',
+      ]
+    },
+    {
+      stepName: 'Money Released',
+      tasks: [
+        'Money released by Government',
+      ]
+    }
+    
+  ];
+
   constructor() { }
 
   ngOnInit() {
